c94d3978073d7678adc82487b3d7bc7aa26395ec
scotchfield <swrittenb@gmail.com>
2014-07-09 12:54:03 -0400

    Remove magic numbers.
    Starting to fix the registration process.

--- src/game-login.php
+++ src/game-login.php
@@ -3,6 +3,16 @@
 require( dirname( __FILE__ ) . '/game-config.php' );
 require( GAME_PATH . 'game-load.php' );
 
+define( 'GAME_LOGIN_NOTIFY_NO_USERNAME',         1 );
+define( 'GAME_LOGIN_NOTIFY_NO_PASSWORD',         2 );
+define( 'GAME_LOGIN_NOTIFY_NO_EMAIL',            3 );
+define( 'GAME_LOGIN_NOTIFY_USERNAME_EXISTS',     4 );
+define( 'GAME_LOGIN_NOTIFY_EMAIL_EXISTS',        5 );
+define( 'GAME_LOGIN_NOTIFY_BAD_USERPASS',        6 );
+
+define( 'GAME_LOGIN_NOTIFY_VALIDATE_NEEDED',   100 );
+
+
 if ( ! isset( $_POST[ 'action' ] ) ) {
     header( 'Location: ' . GAME_URL );
     exit;
@@ -13,13 +23,15 @@ switch ( $_POST[ 'action' ] ) {
         $user = get_user_by_name( $_POST[ 'user' ] );
         if ( FALSE == $user ) {
             // User doesn't exist.
-            header( 'Location: ' . GAME_URL . '?err=5' );
+            header( 'Location: ' . GAME_URL . '?notify=' .
+                    GAME_LOGIN_NOTIFY_BAD_USERPASS );
             exit;
         }
 
         if ( ! password_verify( $_POST[ 'pass' ], $user[ 'user_pass' ] ) ) {
             // Password doesn't match
-            header( 'Location: ' . GAME_URL . '?err=6' );
+            header( 'Location: ' . GAME_URL . '?notify=' .
+                    GAME_LOGIN_NOTIFY_BAD_USERPASS );
             exit;
         }
 
@@ -36,32 +48,45 @@ switch ( $_POST[ 'action' ] ) {
     case 'register':
         if ( ! isset( $_POST[ 'user' ] ) ) {
             // User name is not set.
-            header( 'Location: ' . GAME_URL . '?err=1' );
+            header( 'Location: ' . GAME_URL . '?notify=' .
+                    GAME_LOGIN_NOTIFY_NO_USERNAME );
             exit;
         }
         if ( ! isset( $_POST[ 'pass' ] ) ) {
             // Password is not set.
-            header( 'Location: ' . GAME_URL . '?err=2' );
+            header( 'Location: ' . GAME_URL . '?notify=' .
+                    GAME_LOGIN_NOTIFY_NO_PASSWORD );
             exit;
         }
         if ( ! isset( $_POST[ 'email' ] ) ) {
             // Email is not set.
-            header( 'Location: ' . GAME_URL . '?err=3' );
+            header( 'Location: ' . GAME_URL . '?notify=' .
+                    GAME_LOGIN_NOTIFY_NO_EMAIL );
             exit;
         }
 
         $user = get_user_by_name( $_POST[ 'user' ] );
         if ( FALSE != $user ) {
             // User name exists.
-            header( 'Location: ' . GAME_URL . '?err=4' );
+            header( 'Location: ' . GAME_URL . '?notify=' .
+                    GAME_LOGIN_NOTIFY_USERNAME_EXISTS );
+            exit;
+        }
+
+        $user = get_user_by_email( $_POST[ 'email' ] );
+        if ( FALSE != $user ) {
+            // User name exists.
+            header( 'Location: ' . GAME_URL . '?notify=' .
+                    GAME_LOGIN_NOTIFY_EMAIL_EXISTS );
             exit;
         }
 
         $pass = password_hash( $_POST[ 'pass' ], PASSWORD_DEFAULT );
         add_user( $_POST[ 'user' ], $pass, $_POST[ 'email' ] );
 
+        header( 'Location: ' . GAME_URL . '?notify=' .
+                GAME_LOGIN_NOTIFY_VALIDATE_NEEDED );
         // TODO: Add proper validate code: email, and set validate key.
-        echo( 'Okay!  Sending you an email.  Gotta validate.' );
 
         break;
 }
