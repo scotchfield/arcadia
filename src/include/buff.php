94e5478a5f0d4734e68228b7c4616d1bd3dc6df4
scotchfield <swrittenb@gmail.com>
2014-08-05 10:21:07 -0400

    More progress on moving attributes to game_meta.

--- src/include/buff.php
+++ src/include/buff.php
@@ -21,16 +21,18 @@ function get_buff( $id ) {
 
 function get_all_buffs() {
     return db_fetch_all(
-        'SELECT * FROM game_meta WHERE key_type=? ORDER BY id',
-        array( game_meta_type_buff ) );
+        'SELECT * FROM game_meta WHERE key_type=? ORDER BY meta_key',
+        array( game_meta_type_buff ),
+        'meta_key' );
 }
 
 function get_buffs( $character_id ) {
     return db_fetch_all(
         'SELECT * FROM game_meta AS a, character_meta AS c ' .
-            'WHERE a.key_type=? AND a.meta_key=c.meta_key AND ' .
-            'c.character_id=? ORDER BY a.id',
-        array( game_meta_type_buff, $character_id ),
-        'id'
+            'WHERE a.key_type=? AND c.key_type=? AND ' .
+            'a.meta_key=c.meta_key AND c.character_id=? ORDER BY a.meta_key',
+        array( game_meta_type_buff, game_character_meta_type_buff,
+               $character_id ),
+        'meta_key'
     );
 }
--- src/include/item.php
+++ src/include/item.php
@@ -4,44 +4,48 @@ function item_init() {
     if ( ! defined( 'game_meta_type_item' ) ) {
         define( 'game_meta_type_item', 20010 );
     }
+
+    if ( ! defined( 'game_character_meta_type_item' ) ) {
+        define( 'game_character_meta_type_item', 20010 );
+    }
 }
 
 add_action( 'post_load', 'item_init' );
 
 
 function get_item( $id ) {
-    return db_fetch( 'SELECT * FROM items WHERE id=?', array( $id ) );
+    return db_fetch(
+        'SELECT * FROM game_meta WHERE key_type=? AND meta_key=?',
+        array( game_meta_type_item, $id ) );
 }
 
 function get_all_items() {
-    return db_fetch_all( 'SELECT * FROM items ORDER BY id', array() );
+    return db_fetch_all(
+        'SELECT * FROM game_meta WHERE key_type=? ORDER BY meta_key',
+        array( game_meta_type_item ),
+        'meta_key' );
 }
 
 function get_items( $character_id ) {
     return db_fetch_all(
-        'SELECT * FROM items AS a, character_meta AS c ' .
-            'WHERE a.id=c.meta_key AND c.character_id=? ' .
-            'AND c.key_type=? ' .
-            'ORDER BY a.id',
-        array( $character_id, game_meta_type_item ),
-        'id'
+        'SELECT * FROM game_meta AS a, character_meta AS c ' .
+            'WHERE a.key_type=? AND c.key_type=? AND ' .
+            'a.meta_key=c.meta_key AND c.character_id=? ORDER BY a.meta_key',
+        array( game_meta_type_item, game_character_meta_type_item,
+               $character_id ),
+        'meta_key'
     );
 }
 
-function award_item( $item_id ) {
+function award_item( $meta_key, $meta_value ) {
     global $character;
 
     if ( FALSE == $character ) {
         return;
     }
 
-    if ( isset( $character[ 'meta' ][ game_meta_type_item ][
-                    $item_id ] ) ) {
-        return;
-    }
-
     add_character_meta( $character[ 'id' ], game_meta_type_item,
-        $item_id, time() );
+        $meta_key, $meta_value );
 
     do_action( 'award_item',
                array( 'item_id' => $item_id ) );
