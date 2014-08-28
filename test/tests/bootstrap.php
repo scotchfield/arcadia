8bc4671cd0fb1a7a5983417c7fa85e74f3dcf119
scotchfield <swrittenb@gmail.com>
2014-08-27 21:37:00 -0400

    Proper game-config information in bootstrap for test database.

--- test/tests/bootstrap.php
+++ test/tests/bootstrap.php
@@ -1,7 +1,18 @@
 <?php
 
-$time_start = microtime( $get_as_float = TRUE );
+// game-config.php options for custom testing environment
+define( 'GAME_NAME', 'Arcadia Testing' );
+define( 'GAME_EMAIL', 'scott@scootah.com' );
+define( 'GAME_PATH', '../src/' );
+define( 'GAME_URL', 'http://localhost:8888/arcadia/src/' );
 
-require( '../src/game-config.php' );
+define( 'DB_ADDRESS', 'localhost' );
+define( 'DB_PORT', 8889 );
+define( 'DB_NAME', 'game_test' );
+define( 'DB_USER', 'root' );
+define( 'DB_PASSWORD', 'root' );
+
+
+// load the arcadia environment
 require( GAME_PATH . 'game-load.php' );
 
