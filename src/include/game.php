b783fc4ead6eae9b085bf790fe83c097b0c428a6
scotchfield <swrittenb@gmail.com>
2014-11-02 22:18:01 -0500

    Starting push to component-based architecture.

--- src/include/game.php
+++ src/include/game.php
@@ -3,12 +3,29 @@
 class Arcadia_Game {
     private $game_action = '';
 
+    private $components = array();
+
     public function get_action() {
         return $this->game_action;
     }
 
     public function set_action( $action ) {
         $this->game_action = $action;
+    }
+
+    public function set_component( $component_id, $component ) {
+        $this->components[ $component_id ] = $component;
+    }
+
+    public function get_component( $component_id ) {
+        if ( isset( $this->components[ $component_id ] ) ) {
+            return $this->components[ $component_id ];
+        }
+        return FALSE;
+    }
+
+    public function c( $component_id ) {
+        return $this->get_component( $component_id );
     }
 }
 
