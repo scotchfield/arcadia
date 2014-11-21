b974a3215016a4e0f2a7f7fe44919f0bec3f8c7e
scotchfield <swrittenb@gmail.com>
2014-11-21 00:17:59 -0500

    Tests for item component

--- test/tests/include/test_item.php
+++ test/tests/include/test_item.php
@@ -2,7 +2,26 @@
 
 class TestArcadiaItem extends PHPUnit_Framework_TestCase {
 
+    public function setUp() {
+        $component = new ArcadiaItem();
+
+        do_state( 'post_load' );
+
+        db_execute(
+            'INSERT INTO game_meta ( key_type, meta_key, meta_value ) ' .
+                'VALUES ( ?, 1, "test" )',
+            array( $component->get_flag_game_meta() ) );
+
+        $GLOBALS[ 'character' ] = array( 'id' => 1 );
+    }
+
+    public function tearDown() {
+        db_execute( 'DELETE FROM character_meta', array() );
+        db_execute( 'DELETE FROM game_meta', array() );
+    }
+
     /**
+     * @covers ArcadiaItem::__construct
      * @covers ArcadiaItem::get_flag_game_meta
      */
     public function test_buff_get_flag_game_meta() {
@@ -12,6 +31,7 @@ class TestArcadiaItem extends PHPUnit_Framework_TestCase {
     }
 
     /**
+     * @covers ArcadiaItem::__construct
      * @covers ArcadiaItem::get_flag_character_meta
      */
     public function test_buff_get_flag_character_meta() {
@@ -19,5 +39,52 @@ class TestArcadiaItem extends PHPUnit_Framework_TestCase {
 
         $this->assertNotNull( $component->get_flag_character_meta() );
     }
+
+    /**
+     * @covers ArcadiaItem::get_item
+     */
+    public function test_get_item_empty() {
+        $component = new ArcadiaItem();
+
+        $item = $component->get_item( -1 );
+
+        $this->assertEquals( FALSE, $item );
+    }
+
+    /**
+     * @covers ArcadiaItem::get_item
+     */
+    public function test_get_item_working() {
+        $component = new ArcadiaItem();
+
+        $item = $component->get_item( 1 );
+
+        $this->assertNotEquals( FALSE, $item );
+        $this->assertEquals( 'test', $item[ 'meta_value' ] );
+    }
+
+    /**
+     * @covers ArcadiaItem::get_all_items
+     */
+    public function test_get_all_items() {
+        $component = new ArcadiaItem();
+
+        $items = $component->get_all_items();
+
+        $this->assertCount( 1, $items );
+        $this->assertEquals( 'test', $items[ 1 ][ 'meta_value' ] );
+    }
+
+    /**
+     * @covers ArcadiaItem::get_items
+     */
+    public function test_get_items_empty() {
+        $component = new ArcadiaItem();
+
+        $items = $component->get_items( 1 );
+
+        $this->assertEmpty( $items );
+    }
+
 
 }
