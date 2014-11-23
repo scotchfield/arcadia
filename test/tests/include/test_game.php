7f8c3d0318defd150953613c339ff90218ee62a9
scotchfield <swrittenb@gmail.com>
2014-11-23 14:23:44 -0500

    Unit tests for get and set state args

--- test/tests/include/test_game.php
+++ test/tests/include/test_game.php
@@ -20,4 +20,28 @@ class TestArcadiaGame extends PHPUnit_Framework_TestCase {
     	$this->assertEquals( $test, $result );
     }
 
+    /**
+     * @covers ArcadiaGame::get_state_arg
+     * @covers ArcadiaGame::set_state_arg
+     */
+    public function test_game_set_and_get_state_arg() {
+        $game = new ArcadiaGame();
+
+        $arg_k = 'test_k';
+        $arg_v = 'test_v';
+
+        $game->set_state_arg( $arg_k, $arg_v );
+
+        $this->assertEquals( $arg_v, $game->get_state_arg( $arg_k ) );
+    }
+
+    /**
+     * @covers ArcadiaGame::get_state_arg
+     */
+    public function test_game_get_state_arg_not_set() {
+        $game = new ArcadiaGame();
+
+        $this->assertFalse( $game->get_state_arg( 'test_not_set' ) );
+    }
+
 }
