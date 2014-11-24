32b86ef2ae32ea3c97b3821ec63387ba5df98055
scotchfield <swrittenb@gmail.com>
2014-11-23 20:52:33 -0500

    Heartbeat unit testing

--- test/tests/include/test_heartbeat.php
+++ test/tests/include/test_heartbeat.php
@@ -7,6 +7,10 @@ class TestArcadiaHeartbeat extends PHPUnit_Framework_TestCase {
 
         $GLOBALS[ 'character' ] = array( 'id' => 1 );
 
+        db_execute( 'INSERT INTO characters ' .
+            '( id, user_id, character_name ) VALUES ' .
+            '( 1, 1, \'test\' )' );
+
         db_execute( 'DELETE FROM character_meta WHERE character_id=1 ' .
             'AND key_type=?',
             array( $component->get_flag_character_meta() ) );
@@ -14,9 +18,12 @@ class TestArcadiaHeartbeat extends PHPUnit_Framework_TestCase {
 
     public function tearDown() {
         unset( $GLOBALS[ 'character' ] );
+
+        db_execute( 'DELETE FROM characters' );
     }
 
     /**
+     * @covers ArcadiaHeartbeat::__construct
      * @covers ArcadiaHeartbeat::get_flag_game_meta
      */
     public function test_buff_get_flag_game_meta() {
@@ -26,6 +33,7 @@ class TestArcadiaHeartbeat extends PHPUnit_Framework_TestCase {
     }
 
     /**
+     * @covers ArcadiaHeartbeat::__construct
      * @covers ArcadiaHeartbeat::get_flag_character_meta
      */
     public function test_buff_get_flag_character_meta() {
@@ -71,6 +79,31 @@ class TestArcadiaHeartbeat extends PHPUnit_Framework_TestCase {
 
         $component->add_heartbeat();
         $result = $component->get_all_heartbeats();
+
+        $this->assertCount( 1, $result );
+    }
+
+    /**
+     * @covers ArcadiaHeartbeat::get_heartbeat_characters
+     */
+    public function test_get_heartbeat_characters_empty() {
+        $component = new ArcadiaHeartbeat();
+
+        $result = $component->get_heartbeat_characters( 1 );
+
+        $this->assertCount( 0, $result );
+    }
+
+    /**
+     * @covers ArcadiaHeartbeat::add_heartbeat
+     * @covers ArcadiaHeartbeat::get_heartbeat_characters
+     */
+    public function test_get_heartbeat_characters_single() {
+        $component = new ArcadiaHeartbeat();
+
+        $component->add_heartbeat();
+
+        $result = $component->get_heartbeat_characters( 60 );
 
         $this->assertCount( 1, $result );
     }
