88ae0785417c8142c4fc65e5b8ca5174a5d8d9a8
scotchfield <swrittenb@gmail.com>
2014-11-30 13:20:58 -0500

    NPC component (either in-game conversation, combat, or whatever)

---
+++ src/include/npc.php
@@ -0,0 +1,23 @@
+<?php
+
+class ArcadiaNpc extends ArcadiaComponent {
+
+    function __construct( $key_type = 208 ) {
+        $this->flag_game_meta = $key_type;
+        $this->flag_character_meta = $key_type;
+    }
+
+    public function get_npc( $id ) {
+        return db_fetch(
+            'SELECT * FROM game_meta WHERE key_type=? AND meta_key=?',
+            array( $this->flag_game_meta, $id ) );
+    }
+
+    public function get_all_npcs() {
+        return db_fetch_all(
+            'SELECT * FROM game_meta WHERE key_type=?',
+            array( $this->flag_game_meta ),
+            $assoc = 'meta_key' );
+    }
+
+}
