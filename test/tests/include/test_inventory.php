f4b73598b8868fdfbfef60eee6c34a1757e3f3b2
scotchfield <swrittenb@gmail.com>
2014-12-11 21:35:55 -0500

    Fill out testing coverage for inventory component

--- test/tests/include/test_inventory.php
+++ test/tests/include/test_inventory.php
@@ -2,6 +2,22 @@
 
 class TestArcadiaInventory extends PHPUnit_Framework_TestCase {
 
+    public function setUp() {
+        $component = new ArcadiaInventory();
+
+        do_action( 'post_load' );
+
+        db_execute(
+            'INSERT INTO character_meta ' .
+                '( character_id, key_type, meta_key, meta_value ) ' .
+                'VALUES ( 1, ?, 0, "test" )',
+            array( $component->get_flag_game_meta() ) );
+    }
+
+    public function tearDown() {
+        db_execute( 'DELETE FROM character_meta', array() );
+    }
+
     /**
      * @covers ArcadiaInventory::__construct
      */
@@ -10,5 +26,77 @@ class TestArcadiaInventory extends PHPUnit_Framework_TestCase {
 
         $this->assertNotNull( $component->get_flag_game_meta() );
     }
+
+    /**
+     * @covers ArcadiaInventory::get_inventory
+     */
+    public function test_inventory_get_inventory_single() {
+        $component = new ArcadiaInventory();
+
+        $result = $component->get_inventory( 1 );
+
+        $this->assertCount( 1, $result );
+        $this->assertEquals( $result[ 0 ],
+            array( 'character_id' => '1',
+                   'key_type' => '209',
+                   'meta_key' => '0',
+                   'meta_value' => 'test' ) );
+    }
+
+    /**
+     * @covers ArcadiaInventory::get_inventory
+     */
+    public function test_inventory_get_inventory_empty() {
+        $component = new ArcadiaInventory();
+
+        db_execute( 'DELETE FROM character_meta', array() );
+
+        $result = $component->get_inventory( 1 );
+
+        $this->assertCount( 0, $result );
+    }
+
+    /**
+     * @covers ArcadiaInventory::award_item
+     */
+    public function test_inventory_award_item() {
+        $component = new ArcadiaInventory();
+
+        $result = $component->award_item( 1, 'test_new_item' );
+
+        $this->assertTrue( $result );
+
+        $result = $component->get_inventory( 1 );
+
+        $this->assertCount( 2, $result );
+    }
+
+    /**
+     * @covers ArcadiaInventory::remove_item
+     */
+    public function test_inventory_remove_item_exists() {
+        $component = new ArcadiaInventory();
+
+        $result = $component->remove_item( 1, 0 );
+
+        $this->assertTrue( $result );
+
+        $result = $component->get_inventory( 1 );
+
+        $this->assertCount( 0, $result );
+    }
+
+    /**
+     * @covers ArcadiaInventory::remove_item
+     */
+    public function test_inventory_remove_item_not_exists() {
+        $component = new ArcadiaInventory();
+
+        $result = $component->remove_item( 1, 99 );
+        $result = $component->get_inventory( 1 );
+
+        $this->assertCount( 1, $result );
+    }
+
 
 }
